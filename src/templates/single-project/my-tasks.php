d0c27fc32933bcc8c644958f65fe9974e68f1d89
Denison Martins <denisongmartins@gmail.com>
2017-05-02 15:22:56 -0300

    Fix small text being hidden in some screen sizes

--- src/templates/single-project/my-tasks.php
+++ src/templates/single-project/my-tasks.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
-$sectionTitle = upstream_task_label_plural() . '<small>'. __(' assigned to me', 'upstream') .'</small>';
+$sectionTitle = upstream_task_label_plural() . ' <span class="small">'. __('assigned to me', 'upstream') .'</span>';
 $projectId = get_the_ID();
 $itemAlias = 'tasks';
 ?>
