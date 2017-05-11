69f7f6d2c533850313a453ce74bc2be7e43ec10f
Denison Martins <denisongmartins@gmail.com>
2017-05-11 10:33:35 -0300

    Added function to check if Bugs are disabled for a given project

--- src/includes/up-general-functions.php
+++ src/includes/up-general-functions.php
@@ -549,7 +549,7 @@ function upstream_are_milestones_disabled($post_id = 0)
     }
 
     return $areMilestonesDisabled;
- }
+}
 
 /**
  * Check if Tasks are disabled for the current open project.
@@ -576,4 +576,31 @@ function upstream_are_tasks_disabled($post_id = 0)
     }
 
     return $areTasksDisabled;
- }
+}
+
+/**
+ * Check if Bugs are disabled for the current open project.
+ * If no ID is passed, this function tries to guess it by checking $_GET/$_POST vars.
+ *
+ * @since   1.8.0
+ *
+ * @param   int     $post_id The project ID to be checked
+ *
+ * @return  bool
+ */
+function upstream_are_bugs_disabled($post_id = 0)
+{
+    $areBugsDisabled = false;
+    $post_id = (int)$post_id;
+
+    if ($post_id <= 0) {
+        $post_id = (int)upstream_post_id();
+    }
+
+    if ($post_id > 0) {
+        $theMeta = get_post_meta($post_id, '_upstream_project_disable_bugs', false);
+        $areBugsDisabled = !empty($theMeta) && $theMeta[0] === 'on';
+    }
+
+    return $areBugsDisabled;
+}
