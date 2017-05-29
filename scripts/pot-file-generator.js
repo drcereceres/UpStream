cb416c048cc13f0f13e1c642c2d8ed49eafd3784
Denison Martins <denisongmartins@gmail.com>
2017-05-29 01:00:11 -0300

    Added todo-notes to the pot-file-generator script

--- scripts/pot-file-generator.js
+++ scripts/pot-file-generator.js
@@ -3,6 +3,17 @@ const wpPot = require('wp-pot');
 const PLUGIN_NAME = 'UpStream';
 const LANGUAGE_DOMAIN = 'upstream';
 
+/*
+TODO:
+- Add 'Project-Id-Version' property
+- Add 'Report-Msgid-Bugs-To' property
+- Add 'POT-Creation-Date' property
+- Add 'POT-Revision-Date' property
+- Add 'Last-Translator' property
+- Maybe add 'Language-Team' property
+- Checkout what other properties that might be useful to use
+*/
+
 wpPot({
   destFile: `./src/languages/${LANGUAGE_DOMAIN}.pot`,
   domain: LANGUAGE_DOMAIN,
