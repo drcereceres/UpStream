7147637a22d47021e229408a094acfd0ca87237a
Denison Martins <denisongmartins@gmail.com>
2017-07-04 11:51:31 -0300

    Make the client name field required

--- src/includes/admin/assets/js/metabox-client.js
+++ src/includes/admin/assets/js/metabox-client.js
@@ -1,9 +1,21 @@
 (function(window, document, $, undefined) {
+  'use strict';
+
   if (!$) {
     console.error('UpStream requires jQuery.');
   }
 
+  // Make the Client Name field required.
+  (function() {
+    var titleWrap = $('#titlewrap');
+    var titleLabel = $('#title-prompt-text', titlewrap);
+
+    titleLabel.text(titleLabel.text() + ' *');
+
+    $('#title', titlewrap).attr('required', 'required');
+  })();
+
   $(document).ready(function() {
-    console.log('hfds');
+    // @todo
   });
 })(window, window.document, jQuery || null);
