82069693822ef65b91dbc4ee967276370e7ade5c
Denison Martins <denisongmartins@gmail.com>
2017-07-05 11:18:38 -0300

    Readded metaboxes and code cleanup

--- src/includes/admin/metaboxes/class-up-metaboxes-clients.php
+++ src/includes/admin/metaboxes/class-up-metaboxes-clients.php
@@ -3,6 +3,7 @@
 if (!defined('ABSPATH')) exit;
 
 use \UpStream\Traits\Singleton;
+use \Cmb2Grid\Grid\Cmb2Grid;
 
 // @todo
 class UpStream_Metaboxes_Clients
@@ -10,29 +11,97 @@ class UpStream_Metaboxes_Clients
     use Singleton;
 
     /**
-     * Post type
      * @var string
      * @todo
      */
-    public $type = 'client';
+    protected static $postType = 'client';
 
     /**
-     * Post type
      * @var string
      * @todo
      */
-    public $label = '';
+    protected static $postTypeLabelSingular = null;
 
     /**
-     * Metabox prefix
      * @var string
      * @todo
      */
-    public $prefix = '_upstream_client_';
+    protected static $postTypeLabelPlural = null;
+
+    /**
+     * @todo
+     */
+    protected static $prefix = '_upstream_client_';
 
     public function __construct()
     {
-        $this->label = upstream_client_label();
-        $this->label_plural = upstream_client_label_plural();
+        self::$postTypeLabelSingular = upstream_client_label();
+        self::$postTypeLabelPlural = upstream_client_label_plural();
+
+        self::renderDetailsMetabox();
+        self::renderLogoMetabox();
+    }
+
+    public static function renderDetailsMetabox()
+    {
+        $metabox = new_cmb2_box(array(
+            'id'           => self::$prefix . 'details',
+            'title'        => '<span class="dashicons dashicons-admin-generic"></span>' . __("Details", 'upstream'),
+            'object_types' => array(self::$postType),
+            'context'      => 'side',
+            'priority'     => 'high'
+        ));
+
+        $phoneField = $metabox->add_field(array(
+            'name' => __('Phone Number', 'upstream'),
+            'id'   => self::$prefix . 'phone',
+            'type' => 'text'
+        ));
+
+        $websiteField = $metabox->add_field(array(
+            'name' => __('Website', 'upstream'),
+            'id'   => self::$prefix . 'website',
+            'type' => 'text_url'
+        ));
+
+        // @todo: may we should use tinymce?
+        $addressField = $metabox->add_field(array(
+            'name' => __('Address', 'upstream'),
+            'id'   => self::$prefix . 'address',
+            'type' => 'textarea_small'
+        ));
+
+        // @todo: description/notes field
+
+        $metaboxGrid = new Cmb2Grid($metabox);
+        $metaboxGridRow = $metaboxGrid->addRow(array($phoneField, $websiteField, $addressField));
+        /*
+        $metaboxGridRow->addColumns(array(
+            array($phoneField, 'class' => 'col-md-4'),
+            array($websiteField, 'class' => 'col-md-4')
+        ));
+        */
+        //$metaboxGridRow->addColumns(array($phoneField, $websiteField, $addressField));
+    }
+
+    public static function renderLogoMetabox()
+    {
+        $metabox = new_cmb2_box(array(
+            'id'           => self::$prefix . 'logo',
+            'title'        => '<span class="dashicons dashicons-format-image"></span>' . __("Logo", 'upstream'),
+            'object_types' => array(self::$postType),
+            'context'      => 'side',
+            'priority'     => 'core'
+        ));
+
+        $logoField = $metabox->add_field(array(
+            'name' => __('Logo', 'upstream'),
+            'id'   => self::$prefix . 'logo',
+            'type' => 'file'
+        ));
+
+
+        $metaboxGrid = new Cmb2Grid($metabox);
+        $metaboxGridRow = $metaboxGrid->addRow(array($logoField));
     }
 }
