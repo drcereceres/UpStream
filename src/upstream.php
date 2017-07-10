53814f9f4013250dcb21fdec021dfd7582eaeea2
Denison Martins <denisongmartins@gmail.com>
2017-07-10 19:27:54 -0300

    bump new version

--- src/upstream.php
+++ src/upstream.php
@@ -4,7 +4,7 @@
  * Description: A WordPress Project Management plugin by UpStream.
  * Author: UpStream
  * Author URI: https://upstreamplugin.com
- * Version: 1.10.3b1.0.0
+ * Version: 1.10.3b1.1.0
  * Text Domain: upstream
  * Domain Path: /languages
  */
@@ -121,7 +121,7 @@ final class UpStream
         $this->define( 'UPSTREAM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
         $this->define( 'UPSTREAM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
         $this->define( 'UPSTREAM_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-        $this->define( 'UPSTREAM_VERSION', '1.10.3b1.0.0' );
+        $this->define( 'UPSTREAM_VERSION', '1.10.3b1.1.0' );
     }
 
     /**
