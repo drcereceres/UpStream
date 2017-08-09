ab6aeeb253c0bf5ef82bf0da7090c0def6358994
Manny Rodrigues <endurtech@users.noreply.github.com>
2017-08-09 17:22:39 -0400

    Update up-general-functions.php

--- src/includes/up-general-functions.php
+++ src/includes/up-general-functions.php
@@ -565,7 +565,7 @@ function upstream_admin_email() {
     $option = get_option( 'upstream_general' );
     return isset( $option['admin_email'] ) ? $option['admin_email'] : '';
 }
-function upstream_support_link() {
+function upstream_admin_support() {
    	if( isset( $option['admin_support_link'] ) ) {
 		return isset( $option['admin_support_link'] ) ? $option['admin_support_link'] : '';
 	} else {
