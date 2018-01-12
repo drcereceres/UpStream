386be6b33bca143ebf2e33a4b69b7700aae0012a
Denison Martins <denisongmartins@gmail.com>
2018-01-12 17:54:16 -0200

    Fix filtering ux on extra small screens

--- src/templates/single-project/milestones.php
+++ src/templates/single-project/milestones.php
@@ -68,74 +68,114 @@ $areCommentsEnabled = upstreamAreCommentsEnabledOnMilestones();
     <div class="x_content" style="display: <?php echo $collapseBox ? 'none' : 'block'; ?>;">
       <div class="c-data-table table-responsive">
         <form class="form-inline c-data-table__filters" data-target="#milestones">
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-search"></i>
+          <div class="hidden-xs">
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-search"></i>
+                </div>
+                <input type="search" class="form-control" placeholder="<?php echo $l['LB_MILESTONE']; ?>" data-column="milestone" data-compare-operator="contains">
               </div>
-              <input type="search" class="form-control" placeholder="<?php echo $l['LB_MILESTONE']; ?>" data-column="milestone" data-compare-operator="contains">
+            </div>
+            <div class="form-group">
+              <div class="btn-group">
+                <a href="#milestones-filters" role="button" class="btn btn-default" data-toggle="collapse" aria-expanded="false" aria-controls="milestones-filters">
+                  <i class="fa fa-filter"></i> <?php _e('Toggle Filters', 'upstream'); ?>
+                </a>
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fa fa-download"></i> <?php _e('Export', 'upstream'); ?>
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right">
+                  <li>
+                    <a href="#" data-action="export" data-type="txt">
+                      <i class="fa fa-file-text-o"></i>&nbsp;&nbsp;<?php _e('Plain Text', 'upstream'); ?>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" data-action="export" data-type="csv">
+                      <i class="fa fa-file-code-o"></i>&nbsp;&nbsp;<?php _e('CSV', 'upstream'); ?>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-user"></i>
+          <div class="visible-xs">
+            <div>
+              <a href="#milestones-filters" role="button" class="btn btn-default" data-toggle="collapse" aria-expanded="false" aria-controls="milestones-filters">
+                <i class="fa fa-filter"></i> <?php _e('Toggle Filters', 'upstream'); ?>
+              </a>
+              <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fa fa-download"></i> <?php _e('Export', 'upstream'); ?>
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right">
+                  <li>
+                    <a href="#" data-action="export" data-type="txt">
+                      <i class="fa fa-file-text-o"></i>&nbsp;&nbsp;<?php _e('Plain Text', 'upstream'); ?>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" data-action="export" data-type="csv">
+                      <i class="fa fa-file-code-o"></i>&nbsp;&nbsp;<?php _e('CSV', 'upstream'); ?>
+                    </a>
+                  </li>
+                </ul>
               </div>
-              <select class="form-control o-select2" data-column="assigned_to" data-placeholder="<?php _e('Assignee', 'upstream'); ?>">
-                <option value></option>
-                <option value="__none__"><?php _e('Nobody', 'upstream'); ?></option>
-                <option value="<?php echo $currentUserId; ?>"><?php _e('Me', 'upstream'); ?></option>
-                <optgroup label="<?php _e('Users'); ?>">
-                  <?php foreach ($users as $user_id => $userName): ?>
-                    <?php if ($user_id === $currentUserId) continue; ?>
-                    <option value="<?php echo $user_id; ?>"><?php echo $userName; ?></option>
-                    <?php endforeach; ?>
-                </optgroup>
-              </select>
             </div>
           </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-calendar"></i>
+          <div id="milestones-filters" class="collapse">
+            <div class="form-group visible-xs">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-search"></i>
+                </div>
+                <input type="search" class="form-control" placeholder="<?php echo $l['LB_MILESTONE']; ?>" data-column="milestone" data-compare-operator="contains">
               </div>
-              <input type="text" class="form-control o-datepicker" placeholder="<?php echo $l['LB_START_DATE']; ?>" id="milestones-filter-start_date">
             </div>
-            <input type="hidden" id="milestones-filter-start_date_timestamp" data-column="start_date" data-compare-operator=">=">
-          </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-calendar"></i>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-user"></i>
+                </div>
+                <select class="form-control o-select2" data-column="assigned_to" data-placeholder="<?php _e('Assignee', 'upstream'); ?>">
+                  <option value></option>
+                  <option value="__none__"><?php _e('Nobody', 'upstream'); ?></option>
+                  <option value="<?php echo $currentUserId; ?>"><?php _e('Me', 'upstream'); ?></option>
+                  <optgroup label="<?php _e('Users'); ?>">
+                    <?php foreach ($users as $user_id => $userName): ?>
+                      <?php if ($user_id === $currentUserId) continue; ?>
+                      <option value="<?php echo $user_id; ?>"><?php echo $userName; ?></option>
+                      <?php endforeach; ?>
+                  </optgroup>
+                </select>
               </div>
-              <input type="text" class="form-control o-datepicker" placeholder="<?php echo $l['LB_END_DATE']; ?>" id="milestones-filter-end_date">
             </div>
-            <input type="hidden" id="milestones-filter-end_date_timestamp" data-column="end_date" data-compare-operator="<=">
-          </div>
-          <div class="form-group">
-            <div class="btn-group">
-              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-download"></i>
-                <span class="caret"></span>
-              </button>
-              <ul class="dropdown-menu dropdown-menu-right">
-                <li>
-                  <a href="#" data-action="export" data-type="txt">
-                    <i class="fa fa-file-text-o"></i>&nbsp;&nbsp;<?php _e('Plain Text', 'upstream'); ?>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" data-action="export" data-type="csv">
-                    <i class="fa fa-file-code-o"></i>&nbsp;&nbsp;<?php _e('CSV', 'upstream'); ?>
-                  </a>
-                </li>
-              </ul>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-calendar"></i>
+                </div>
+                <input type="text" class="form-control o-datepicker" placeholder="<?php echo $l['LB_START_DATE']; ?>" id="milestones-filter-start_date">
+              </div>
+              <input type="hidden" id="milestones-filter-start_date_timestamp" data-column="start_date" data-compare-operator=">=">
+            </div>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-calendar"></i>
+                </div>
+                <input type="text" class="form-control o-datepicker" placeholder="<?php echo $l['LB_END_DATE']; ?>" id="milestones-filter-end_date">
+              </div>
+              <input type="hidden" id="milestones-filter-end_date_timestamp" data-column="end_date" data-compare-operator="<=">
             </div>
           </div>
         </form>
         <table
           id="milestones"
-          class="o-data-table table table-striped table-bordered table-responsive is-orderable"
+          class="o-data-table table table-bordered table-responsive table-hover is-orderable"
           cellspacing="0"
           width="100%"
           data-type="milestone"
