cdc0af5665f9227b096a6abaa80adc0689060ed5
Denison Martins <denisongmartins@gmail.com>
2018-01-15 11:59:31 -0200

    Make filters more responsive on Tasks

--- src/templates/single-project/tasks.php
+++ src/templates/single-project/tasks.php
@@ -95,100 +95,140 @@ $areCommentsEnabled = upstreamAreCommentsEnabledOnTasks();
     <div class="x_content" style="display: <?php echo $collapseBox ? 'none' : 'block'; ?>;">
       <div class="c-data-table table-responsive">
         <form class="form-inline c-data-table__filters" data-target="#tasks">
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-search"></i>
+          <div class="hidden-xs">
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-search"></i>
+                </div>
+                <input type="search" class="form-control" placeholder="<?php echo $l['LB_TITLE']; ?>" data-column="title" data-compare-operator="contains">
               </div>
-              <input type="search" class="form-control" placeholder="<?php echo $l['LB_TITLE']; ?>" data-column="title" data-compare-operator="contains">
+            </div>
+            <div class="form-group">
+              <div class="btn-group">
+                <a href="#tasks-filters" role="button" class="btn btn-default" data-toggle="collapse" aria-expanded="false" aria-controls="tasks-filters">
+                  <i class="fa fa-filter"></i> <?php _e('Toggle Filters', 'upstream'); ?>
+                </a>
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fa fa-download"></i> <?php _e('Export', 'upstream'); ?>
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right">
+                  <li>
+                    <a href="#" data-action="export" data-type="txt">
+                      <i class="fa fa-file-text-o"></i>&nbsp;&nbsp;<?php _e('Plain Text', 'upstream'); ?>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" data-action="export" data-type="csv">
+                      <i class="fa fa-file-code-o"></i>&nbsp;&nbsp;<?php _e('CSV', 'upstream'); ?>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-user"></i>
+          <div class="visible-xs">
+            <div>
+              <a href="#tasks-filters" role="button" class="btn btn-default" data-toggle="collapse" aria-expanded="false" aria-controls="tasks-filters">
+                <i class="fa fa-filter"></i> <?php _e('Toggle Filters', 'upstream'); ?>
+              </a>
+              <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fa fa-download"></i> <?php _e('Export', 'upstream'); ?>
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right">
+                  <li>
+                    <a href="#" data-action="export" data-type="txt">
+                      <i class="fa fa-file-text-o"></i>&nbsp;&nbsp;<?php _e('Plain Text', 'upstream'); ?>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" data-action="export" data-type="csv">
+                      <i class="fa fa-file-code-o"></i>&nbsp;&nbsp;<?php _e('CSV', 'upstream'); ?>
+                    </a>
+                  </li>
+                </ul>
               </div>
-              <select class="form-control o-select2" data-column="assigned_to" data-placeholder="<?php _e('Assignee', 'upstream'); ?>">
-                <option value></option>
-                <option value="__none__"><?php _e('Nobody', 'upstream'); ?></option>
-                <option value="<?php echo $currentUserId; ?>"><?php _e('Me', 'upstream'); ?></option>
-                <optgroup label="<?php _e('Users'); ?>">
-                  <?php foreach ($users as $user_id => $userName): ?>
-                    <?php if ($user_id === $currentUserId) continue; ?>
-                    <option value="<?php echo $user_id; ?>"><?php echo $userName; ?></option>
+            </div>
+          </div>
+          <div id="tasks-filters" class="collapse">
+            <div class="form-group visible-xs">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-search"></i>
+                </div>
+                <input type="search" class="form-control" placeholder="<?php echo $l['LB_TITLE']; ?>" data-column="title" data-compare-operator="contains">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-user"></i>
+                </div>
+                <select class="form-control o-select2" data-column="assigned_to" data-placeholder="<?php _e('Assignee', 'upstream'); ?>">
+                  <option value></option>
+                  <option value="__none__"><?php _e('Nobody', 'upstream'); ?></option>
+                  <option value="<?php echo $currentUserId; ?>"><?php _e('Me', 'upstream'); ?></option>
+                  <optgroup label="<?php _e('Users'); ?>">
+                    <?php foreach ($users as $user_id => $userName): ?>
+                      <?php if ($user_id === $currentUserId) continue; ?>
+                      <option value="<?php echo $user_id; ?>"><?php echo $userName; ?></option>
+                      <?php endforeach; ?>
+                  </optgroup>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-bookmark"></i>
+                </div>
+                <select class="form-control o-select2" data-column="status" data-placeholder="<?php _e('Status', 'upstream'); ?>">
+                  <option value></option>
+                  <option value="__none__"><?php _e('None', 'upstream'); ?></option>
+                  <optgroup label="<?php _e('Status', 'upstream'); ?>">
+                    <?php foreach ($tasksStatuses as $status): ?>
+                    <option value="<?php echo $status['name']; ?>"><?php echo $status['name']; ?></option>
                     <?php endforeach; ?>
-                </optgroup>
-              </select>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-bookmark"></i>
+                  </optgroup>
+                </select>
               </div>
-              <select class="form-control o-select2" data-column="status" data-placeholder="<?php _e('Status', 'upstream'); ?>">
-                <option value></option>
-                <option value="__none__"><?php _e('None', 'upstream'); ?></option>
-                <optgroup label="<?php _e('Status', 'upstream'); ?>">
-                  <?php foreach ($tasksStatuses as $status): ?>
-                  <option value="<?php echo $status['name']; ?>"><?php echo $status['name']; ?></option>
-                  <?php endforeach; ?>
-                </optgroup>
-              </select>
             </div>
-          </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-flag"></i>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-flag"></i>
+                </div>
+                <select class="form-control o-select2" data-column="milestone" data-placeholder="<?php echo $l['LB_MILESTONE']; ?>">
+                  <option value></option>
+                  <option value="__none__"><?php _e('None', 'upstream'); ?></option>
+                  <optgroup label="<?php echo upstream_milestone_label_plural(); ?>">
+                    <?php foreach ($milestones as $milestone_id => $milestone): ?>
+                    <option value="<?php echo $milestone_id; ?>"><?php echo $milestone; ?></option>
+                    <?php endforeach; ?>
+                  </optgroup>
+                </select>
               </div>
-              <select class="form-control o-select2" data-column="milestone" data-placeholder="<?php echo $l['LB_MILESTONE']; ?>">
-                <option value></option>
-                <option value="__none__"><?php _e('None', 'upstream'); ?></option>
-                <optgroup label="<?php echo upstream_milestone_label_plural(); ?>">
-                  <?php foreach ($milestones as $milestone_id => $milestone): ?>
-                  <option value="<?php echo $milestone_id; ?>"><?php echo $milestone; ?></option>
-                  <?php endforeach; ?>
-                </optgroup>
-              </select>
             </div>
-          </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-calendar"></i>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-calendar"></i>
+                </div>
+                <input type="text" class="form-control o-datepicker" placeholder="<?php echo $l['LB_START_DATE']; ?>" id="tasks-filter-start_date">
               </div>
-              <input type="text" class="form-control o-datepicker" placeholder="<?php echo $l['LB_START_DATE']; ?>" id="tasks-filter-start_date">
+              <input type="hidden" id="tasks-filter-start_date_timestamp" data-column="start_date" data-compare-operator=">=">
             </div>
-            <input type="hidden" id="tasks-filter-start_date_timestamp" data-column="start_date" data-compare-operator=">=">
-          </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-calendar"></i>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-calendar"></i>
+                </div>
+                <input type="text" class="form-control o-datepicker" placeholder="<?php echo $l['LB_END_DATE']; ?>" id="tasks-filter-end_date">
               </div>
-              <input type="text" class="form-control o-datepicker" placeholder="<?php echo $l['LB_END_DATE']; ?>" id="tasks-filter-end_date">
-            </div>
-            <input type="hidden" id="tasks-filter-end_date_timestamp" data-column="end_date" data-compare-operator="<=">
-          </div>
-          <div class="form-group">
-            <div class="btn-group">
-              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-download"></i>
-                <span class="caret"></span>
-              </button>
-              <ul class="dropdown-menu dropdown-menu-right">
-                <li>
-                  <a href="#" data-action="export" data-type="txt">
-                    <i class="fa fa-file-text-o"></i>&nbsp;&nbsp;<?php _e('Plain Text', 'upstream'); ?>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" data-action="export" data-type="csv">
-                    <i class="fa fa-file-code-o"></i>&nbsp;&nbsp;<?php _e('CSV', 'upstream'); ?>
-                  </a>
-                </li>
-              </ul>
+              <input type="hidden" id="tasks-filter-end_date_timestamp" data-column="end_date" data-compare-operator="<=">
             </div>
           </div>
         </form>
