3423af828cb66061475926b77d66451af4709283
Denison Martins <denisongmartins@gmail.com>
2018-01-15 12:06:34 -0200

    Make filters more responsive on Files

--- src/templates/single-project/files.php
+++ src/templates/single-project/files.php
@@ -62,58 +62,98 @@ $areCommentsEnabled = upstreamAreCommentsEnabledOnFiles();
     <div class="x_content" style="display: <?php echo $collapseBox ? 'none' : 'block'; ?>;">
       <div class="c-data-table table-responsive">
         <form class="form-inline c-data-table__filters" data-target="#files">
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-search"></i>
+          <div class="hidden-xs">
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-search"></i>
+                </div>
+                <input type="search" class="form-control" placeholder="<?php echo $l['LB_TITLE']; ?>" data-column="title" data-compare-operator="contains">
               </div>
-              <input type="search" class="form-control" placeholder="<?php echo $l['LB_TITLE']; ?>" data-column="title" data-compare-operator="contains">
+            </div>
+            <div class="form-group">
+              <div class="btn-group">
+                <a href="#files-filters" role="button" class="btn btn-default" data-toggle="collapse" aria-expanded="false" aria-controls="files-filters">
+                  <i class="fa fa-filter"></i> <?php _e('Toggle Filters', 'upstream'); ?>
+                </a>
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fa fa-download"></i> <?php _e('Export', 'upstream'); ?>
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right">
+                  <li>
+                    <a href="#" data-action="export" data-type="txt">
+                      <i class="fa fa-file-text-o"></i>&nbsp;&nbsp;<?php _e('Plain Text', 'upstream'); ?>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" data-action="export" data-type="csv">
+                      <i class="fa fa-file-code-o"></i>&nbsp;&nbsp;<?php _e('CSV', 'upstream'); ?>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-user"></i>
+          <div class="visible-xs">
+            <div>
+              <a href="#files-filters" role="button" class="btn btn-default" data-toggle="collapse" aria-expanded="false" aria-controls="files-filters">
+                <i class="fa fa-filter"></i> <?php _e('Toggle Filters', 'upstream'); ?>
+              </a>
+              <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fa fa-download"></i> <?php _e('Export', 'upstream'); ?>
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right">
+                  <li>
+                    <a href="#" data-action="export" data-type="txt">
+                      <i class="fa fa-file-text-o"></i>&nbsp;&nbsp;<?php _e('Plain Text', 'upstream'); ?>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" data-action="export" data-type="csv">
+                      <i class="fa fa-file-code-o"></i>&nbsp;&nbsp;<?php _e('CSV', 'upstream'); ?>
+                    </a>
+                  </li>
+                </ul>
               </div>
-              <select class="form-control o-select2" data-column="created_by" data-placeholder="<?php _e('Uploader', 'upstream'); ?>">
-                <option value></option>
-                <option value="<?php echo $currentUserId; ?>"><?php _e('Me', 'upstream'); ?></option>
-                <optgroup label="<?php _e('Users'); ?>">
-                  <?php foreach ($users as $user_id => $userName): ?>
-                    <?php if ($user_id === $currentUserId) continue; ?>
-                    <option value="<?php echo $user_id; ?>"><?php echo $userName; ?></option>
-                    <?php endforeach; ?>
-                </optgroup>
-              </select>
             </div>
           </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-calendar"></i>
+          <div id="files-filters" class="collapse">
+            <div class="form-group visible-xs">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-search"></i>
+                </div>
+                <input type="search" class="form-control" placeholder="<?php echo $l['LB_TITLE']; ?>" data-column="title" data-compare-operator="contains">
               </div>
-              <input type="text" class="form-control o-datepicker" placeholder="<?php echo $l['LB_UPLOADED_AT']; ?>" id="files-filter-uploaded_at_from">
             </div>
-            <input type="hidden" id="files-filter-uploaded_at_from_timestamp" data-column="created_time" data-compare-operator=">=">
-          </div>
-          <div class="form-group">
-            <div class="btn-group">
-              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-download"></i>
-                <span class="caret"></span>
-              </button>
-              <ul class="dropdown-menu dropdown-menu-right">
-                <li>
-                  <a href="#" data-action="export" data-type="txt">
-                    <i class="fa fa-file-text-o"></i>&nbsp;&nbsp;<?php _e('Plain Text', 'upstream'); ?>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" data-action="export" data-type="csv">
-                    <i class="fa fa-file-code-o"></i>&nbsp;&nbsp;<?php _e('CSV', 'upstream'); ?>
-                  </a>
-                </li>
-              </ul>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-user"></i>
+                </div>
+                <select class="form-control o-select2" data-column="created_by" data-placeholder="<?php _e('Uploader', 'upstream'); ?>">
+                  <option value></option>
+                  <option value="<?php echo $currentUserId; ?>"><?php _e('Me', 'upstream'); ?></option>
+                  <optgroup label="<?php _e('Users'); ?>">
+                    <?php foreach ($users as $user_id => $userName): ?>
+                      <?php if ($user_id === $currentUserId) continue; ?>
+                      <option value="<?php echo $user_id; ?>"><?php echo $userName; ?></option>
+                      <?php endforeach; ?>
+                  </optgroup>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon">
+                  <i class="fa fa-calendar"></i>
+                </div>
+                <input type="text" class="form-control o-datepicker" placeholder="<?php echo $l['LB_UPLOADED_AT']; ?>" id="files-filter-uploaded_at_from">
+              </div>
+              <input type="hidden" id="files-filter-uploaded_at_from_timestamp" data-column="created_time" data-compare-operator=">=">
             </div>
           </div>
         </form>
