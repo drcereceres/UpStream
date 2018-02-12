f7d73c16d4c0ab6f00f4f87f65e3a33aad29b430
Denison Martins <denisongmartins@gmail.com>
2018-02-12 16:19:14 -0200

    Bump new version

--- src/readme.txt
+++ src/readme.txt
@@ -136,7 +136,7 @@ UpStream does not use the existing styling of your theme. The features and the v
 The format is based on [Keep a Changelog](http://keepachangelog.com)
 and this project adheres to [Semantic Versioning](http://semver.org).
 
-= [1.14.1] - @todo =
+= [1.14.1] - 2018-02-12 =
 
 Fixed:
 * Fixed CMB2 not being loaded correctly in a multisite environment
--- src/upstream.php
+++ src/upstream.php
@@ -187,7 +187,7 @@ final class UpStream
         $this->define( 'UPSTREAM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
         $this->define( 'UPSTREAM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
         $this->define( 'UPSTREAM_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-        $this->define( 'UPSTREAM_VERSION', '1.14.1-' . mt_rand(1, 99999) ); // @todo
+        $this->define( 'UPSTREAM_VERSION', '1.14.1' );
     }
 
     /**
