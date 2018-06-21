b73154b42d35a24f44f94c7fff805f9834a28037
Anderson Grudtner Martins <anderson@ostraining.com>
2018-06-21 17:32:38 -0300

    Update missed version number

--- src/upstream.php
+++ src/upstream.php
@@ -184,7 +184,7 @@ if ( ! class_exists( 'UpStream' ) ) :
             $this->define( 'UPSTREAM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
             $this->define( 'UPSTREAM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
             $this->define( 'UPSTREAM_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-            $this->define( 'UPSTREAM_VERSION', '1.18.3' );
+            $this->define( 'UPSTREAM_VERSION', '1.18.4' );
         }
 
         /**
