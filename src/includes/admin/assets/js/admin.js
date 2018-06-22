4866b4f5c5488ac8c9359c69b1709e1dd6af7ae4
Anderson Grudtner Martins <anderson@ostraining.com>
2018-06-22 12:02:23 -0300

    Fix custom style for Extensions submenu
    The submenu was identified if the UpStream menu wasn’t opened. Now we look for it for some seconds, to wait it load.
    
    AllediaWordPress/UpStream#184

--- src/includes/admin/assets/js/admin.js
+++ src/includes/admin/assets/js/admin.js
@@ -1,10 +1,29 @@
 (function ($) {
-    $('#toplevel_page_upstream_general ul.wp-submenu li').each(function() {
-        if ($(this).find('a').length > 0) {
-            if ($(this).find('a').attr('href') === 'admin.php?page=upstream_extensions') {
-                // Check if the current menu links to the extensions page.
-                $(this).addClass('upstream-extensions');
+    // Add a custom class to the Extensions menu item for custom styling.
+    var menu_found = false,
+        interval,
+        limit = 20,
+        i = 0;
+
+    interval = window.setInterval(
+        function() {
+            if (menu_found || limit === i) {
+                window.clearInterval(interval);
+                return;
             }
-        }
-    });
+
+            i++;
+
+            $('#toplevel_page_upstream_general ul.wp-submenu li').each(function() {
+                if ($(this).find('a').length > 0) {
+                    if ($(this).find('a').attr('href') === 'admin.php?page=upstream_extensions') {
+                        // Check if the current menu links to the extensions page.
+                        $(this).addClass('upstream-extensions');
+                        menu_found = true;
+                    }
+                }
+            });
+        },
+        500
+    );
 })(jQuery);
