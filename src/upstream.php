4a4ab155d44f42872093e73b05b10b18cfeada83
Anderson Grudtner Martins <anderson@alledia.com>
2018-07-04 14:06:14 -0300

    Bump minor version

--- src/upstream.php
+++ src/upstream.php
@@ -4,7 +4,7 @@
  * Description: A WordPress Project Management plugin by UpStream.
  * Author: UpStream
  * Author URI: https://upstreamplugin.com
- * Version: 1.19.0-beta.4
+ * Version: 1.19.0
  * Text Domain: upstream
  * Domain Path: /languages
  */
@@ -218,7 +218,7 @@ if ( ! class_exists( 'UpStream' ) ) :
             $this->define( 'UPSTREAM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
             $this->define( 'UPSTREAM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
             $this->define( 'UPSTREAM_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-            $this->define( 'UPSTREAM_VERSION', '1.19.0-beta.4' );
+            $this->define( 'UPSTREAM_VERSION', '1.19.0' );
         }
 
         /**
