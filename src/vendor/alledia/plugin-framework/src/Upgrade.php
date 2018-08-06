14dfc9e36ea91f0e254246ca35057246df8b3520
Anderson Grudtner Martins <anderson@alledia.com>
2018-08-06 20:52:06 -0300

    Add the AllediaFramework to the vendor folder

---
+++ src/vendor/alledia/plugin-framework/src/Upgrade.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AllediaFramework;
+
+class Upgrade {
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * TextDomain constructor.
+     *
+     * @param Container $container
+     */
+    public function __construct( Container $container ) {
+        $this->container = $container;
+    }
+
+    /**
+     * Add an Upgrade link to the action links in the plugin list.
+     */
+    public function add_action_upgrade_link( $url ) {
+        $this->url = $url;
+
+
+        add_action( 'plugin_action_links_' . $this->container['PLUGIN_BASENAME'], [ $this, 'plugin_action_links' ],
+            999 );
+    }
+
+    /**
+     * @param array $links
+     */
+    public function plugin_action_links( $links ) {
+        $twig = $this->container['twig'];
+
+        $context = [
+            'url'   => $this->url,
+            'label' => __( 'Upgrade', 'alledia-framework' ),
+            'class' => $this->container['PLUGIN_NAME'],
+        ];
+
+        $link = $twig->render( 'action_link_upgrade.twig', $context );
+
+        $links = array_merge( $links, [ $link ] );
+
+        return $links;
+    }
+}
