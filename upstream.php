989ae5f263e64d79053a5ba77ea5e1b4e7ca91f5
Anderson Grudtner Martins <anderson@alledia.com>
2018-10-18 16:02:24 -0300

    Bump stable version to 1.22.1

--- upstream.php
+++ upstream.php
@@ -4,7 +4,7 @@
  * Description: A WordPress Project Management plugin by UpStream.
  * Author: UpStream
  * Author URI: https://upstreamplugin.com
- * Version: 1.22.1-beta.6
+ * Version: 1.22.1
  * Text Domain: upstream
  * Domain Path: /languages
  */
